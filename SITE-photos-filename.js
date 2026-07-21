bb9f33b96f4fdc97ae5671ed33df3dbc9325e290
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-07-21 12:22:36 +0500

    Add files via upload

---
+++ SITE-photos-filename.js
@@ -0,0 +1,15 @@
+export async function onRequestGet({ params, env }) {
+  const key = params.filename;
+  const object = await env.PHOTOS_BUCKET.get(key);
+
+  if (!object) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set("etag", object.httpEtag);
+  headers.set("Cache-Control", "public, max-age=31536000, immutable");
+
+  return new Response(object.body, { headers });
+}
