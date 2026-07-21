ad8d2f43b8be5e0acda9173f9c563eee7e5331ef
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-07-21 23:02:45 +0500

    Delete functions directory

--- functions/api/about/index.js
+++
@@ -1,7 +0,0 @@
-const PUBLIC_SITE = "https://mhbuilderlahore.site";
-
-export async function onRequestGet({ env }) {
-  const row = await env.DB.prepare("SELECT * FROM about WHERE id = 1").first();
-  const withUrl = row ? { ...row, image_url: row.r2_key ? `${PUBLIC_SITE}/photos/${row.r2_key}` : null } : {};
-  return new Response(JSON.stringify({ about: withUrl }), { headers: { "Content-Type": "application/json" } });
-}
--- functions/api/settings/index.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet({ env }) {
-  const row = await env.DB.prepare("SELECT * FROM settings WHERE id = 1").first();
-  return new Response(JSON.stringify({ settings: row || {} }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
